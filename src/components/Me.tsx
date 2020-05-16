25234bcc9967f92fcbe92e63e344ff1b4dfd4d56
David Biro <dbiro97@gmail.com>
2020-05-16 12:39:40 +0200

    change horizontal limit of the 3d image

--- src/components/Me.tsx
+++ src/components/Me.tsx
@@ -1,37 +1,40 @@
-import './Me.scss'
-import React from 'react'
+import "./Me.scss";
+import React from "react";
 
-import img from '../images/me.jpg'
-import imgMap from '../images/me_depth.jpg'
+import img from "../images/me.jpg";
+import imgMap from "../images/me_depth.jpg";
 
 export default () => {
-
-
     React.useEffect(() => {
-        require('../fake3D.js')
-    })
+        require("../fake3D.js");
+    });
 
     let me = React.createRef();
 
-    const handleClick = _e => {
+    const handleClick = (_e) => {
         window.scrollTo({
             top: me.current.scrollHeight,
-            behavior: 'smooth'
-        })
-    }
+            behavior: "smooth",
+        });
+    };
 
-    return <>
-        <section className="me" onClick={handleClick} ref={me}>
-            <div
-                id="gl"
-                data-imageoriginal={img}
-                data-imagedepth={imgMap}
-                data-horizontalthreshold="30"
-                data-verticalthreshold="30"
-            />
+    return (
+        <>
+            <section className="me" onClick={handleClick} ref={me}>
+                <div
+                    id="gl"
+                    data-imageoriginal={img}
+                    data-imagedepth={imgMap}
+                    data-horizontalthreshold="15"
+                    data-verticalthreshold="30"
+                />
 
-            <h1> <span>David Biro</span> Full-stack Web developer </h1>
-        </section>
-        <div className="line"></div>
-    </>
-}
+                <h1>
+                    {" "}
+                    <span>David Biro</span> Full-stack Web developer{" "}
+                </h1>
+            </section>
+            <div className="line"></div>
+        </>
+    );
+};
